0f99e213fa9759a1df2ce591bb3d4431ab9f62e5
Zack Dutke <zdutke@mail.umw.edu>
2024-11-16 11:33:42 -0500

    sprites.c is the preferred file, sprites partially show now, but backgrounds are screwed up

--- tiles.c
+++ tiles.c
@@ -3,12 +3,19 @@
  * program which demonstraes tile mode 0
  */
 
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 160
+
+
 /* include the image we are using */
 #include "battletoads_pink_bg.h"
 
 /* include the tile map we are using */
 #include "level_wall.h"
 #include "level_road.h"
+
+/* include the sprite we are using */
+#include "wall_obstacle.h"
 
 /* the width and height of the screen */
 #define WIDTH 240
@@ -34,11 +41,21 @@ volatile unsigned short* bg3_control = (volatile unsigned short*) 0x400000e;
 /* palette is always 256 colors */
 #define PALETTE_SIZE 256
 
+/*there are 128 sprites on the GBA */
+#define NUM_SPRITES 128
+
 /* the display control pointer points to the gba graphics register */
 volatile unsigned long* display_control = (volatile unsigned long*) 0x4000000;
 
+/* the memory locationw hich controls sprite attributes */
+volatile unsigned short* sprite_attribute_memory = (volatile unsigned short*) 0x7000000;
+
+/* the memory location which stores sprite image data */
+volatile unsigned short* sprite_image_memory = (volatile unsigned short*) 0x6010000;
+
 /* the address of the color palette */
 volatile unsigned short* bg_palette = (volatile unsigned short*) 0x5000000;
+volatile unsigned short* sprite_palette = (volatile unsigned short*) 0x6010000;
 
 /* the button register holds the bits which indicate whether each button has
  * been pressed - this has got to be volatile as well
@@ -107,6 +124,28 @@ volatile unsigned short* screen_block(unsigned long block) {
     return (volatile unsigned short*) (0x6000000 + (block * 0x800));
 }
 
+/* flag for turning on DMA */
+#define DMA_ENABLE 0x80000000
+
+/* flags for the sizes to transfer, 16 or 32 bits */
+#define DMA_16 0x00000000
+#define DMA_32 0x04000000
+
+/* pointer to the DMA source location */
+volatile unsigned int* dma_source = (volatile unsigned int*) 0x40000D4;
+
+/* pointer to the DMA destination location */
+volatile unsigned int* dma_destination = (volatile unsigned int*) 0x40000D8;
+
+/* pointer to the DMA count/control */
+volatile unsigned int* dma_count = (volatile unsigned int*) 0x40000DC;
+
+/* copy data using DMA */
+void memcpy16_dma(unsigned short* dest, unsigned short* source, int amount) {
+    *dma_source = (unsigned int) source;
+    *dma_destination = (unsigned int) dest;
+    *dma_count = amount | DMA_16 | DMA_ENABLE;
+}
 
 /* function to setup background 0 for this program */
 void setup_background() {
@@ -119,7 +158,7 @@ void setup_background() {
     /* load the image into char block 0 (16 bits at a time) */
     volatile unsigned short* dest = char_block(0);
     unsigned short* image = (unsigned short*) battletoads_pink_bg_data;
-    for (int i = 0; i < ((battletoads_pink_bg_width * battletoads_pink_bg_height) / 2); i++) {
+    for (int i = 0; i < (battletoads_pink_bg_width * battletoads_pink_bg_height); i++) {
         dest[i] = image[i];
     }
 
@@ -156,6 +195,191 @@ void delay(unsigned int amount) {
     for (int i = 0; i < amount * 10; i++);
 }
 
+/* a sprite is a moveable image on the screen */
+struct Sprite {
+    unsigned short attribute0;
+    unsigned short attribute1;
+    unsigned short attribute2;
+    unsigned short attribute3;
+};
+
+/* array of all the sprites available on the GBA */
+struct Sprite sprites[NUM_SPRITES];
+int next_sprite_index = 0;
+
+/* the different sizes of sprites which are possible */
+enum SpriteSize {
+    SIZE_8_8,
+    SIZE_16_16,
+    SIZE_32_32,
+    SIZE_64_64,
+    SIZE_16_8,
+    SIZE_32_8,
+    SIZE_32_16,
+    SIZE_64_32,
+    SIZE_8_16,
+    SIZE_8_32,
+    SIZE_16_32,
+    SIZE_32_64
+};
+
+/* function to initialize a sprite with its properties, and return a pointer */
+struct Sprite* sprite_init(int x, int y, enum SpriteSize size,
+        int horizontal_flip, int vertical_flip, int tile_index, int priority) {
+
+    /* grab the next index */
+    int index = next_sprite_index++;
+
+    /* setup the bits used for each shape/size possible */
+    int size_bits, shape_bits;
+    switch (size) {
+        case SIZE_8_8:   size_bits = 0; shape_bits = 0; break;
+        case SIZE_16_16: size_bits = 1; shape_bits = 0; break;
+        case SIZE_32_32: size_bits = 2; shape_bits = 0; break;
+        case SIZE_64_64: size_bits = 3; shape_bits = 0; break;
+        case SIZE_16_8:  size_bits = 0; shape_bits = 1; break;
+        case SIZE_32_8:  size_bits = 1; shape_bits = 1; break;
+        case SIZE_32_16: size_bits = 2; shape_bits = 1; break;
+        case SIZE_64_32: size_bits = 3; shape_bits = 1; break;
+        case SIZE_8_16:  size_bits = 0; shape_bits = 2; break;
+        case SIZE_8_32:  size_bits = 1; shape_bits = 2; break;
+        case SIZE_16_32: size_bits = 2; shape_bits = 2; break;
+        case SIZE_32_64: size_bits = 3; shape_bits = 2; break;
+    }
+
+    int h = horizontal_flip ? 1 : 0;
+    int v = vertical_flip ? 1 : 0;
+
+    /* set up the first attribute */
+    sprites[index].attribute0 = y |             /* y coordinate */
+                            (0 << 8) |          /* rendering mode */
+                            (0 << 10) |         /* gfx mode */
+                            (0 << 12) |         /* mosaic */
+                            (1 << 13) |         /* color mode, 0:16, 1:256 */
+                            (shape_bits << 14); /* shape */
+
+    /* set up the second attribute */
+    sprites[index].attribute1 = x |             /* x coordinate */
+                            (0 << 9) |          /* affine flag */
+                            (h << 12) |         /* horizontal flip flag */
+                            (v << 13) |         /* vertical flip flag */
+                            (size_bits << 14);  /* size */
+
+    /* setup the second attribute */
+    sprites[index].attribute2 = tile_index |   // tile index */
+                            (priority << 10) | // priority */
+                            (0 << 12);         // palette bank (only 16 color)*/
+
+    /* return pointer to this sprite */
+    return &sprites[index];
+}
+
+/* update all of the spries on the screen */
+void sprite_update_all() {
+    /* copy them all over */
+    memcpy16_dma((unsigned short*) sprite_attribute_memory, (unsigned short*) sprites, NUM_SPRITES * 4);
+}
+
+/* setup all sprites */
+void sprite_clear() {
+    /* clear the index counter */
+    next_sprite_index = 0;
+
+    /* move all sprites offscreen to hide them */
+    for(int i = 0; i < NUM_SPRITES; i++) {
+        sprites[i].attribute0 = SCREEN_HEIGHT;
+        sprites[i].attribute1 = SCREEN_WIDTH;
+    }
+}
+
+/* set a sprite postion */
+void sprite_position(struct Sprite* sprite, int x, int y) {
+    /* clear out the y coordinate */
+    sprite->attribute0 &= 0xff00;
+
+    /* set the new y coordinate */
+    sprite->attribute0 |= (y & 0xff);
+
+    /* clear out the x coordinate */
+    sprite->attribute1 &= 0xfe00;
+
+    /* set the new x coordinate */
+    sprite->attribute1 |= (x & 0x1ff);
+}
+
+/* move a sprite in a direction */
+void sprite_move(struct Sprite* sprite, int dx, int dy) {
+    /* get the current y coordinate */
+    int y = sprite->attribute0 & 0xff;
+
+    /* get the current x coordinate */
+    int x = sprite->attribute1 & 0x1ff;
+
+    /* move to the new location */
+    sprite_position(sprite, x + dx, y + dy);
+}
+
+/* change the vertical flip flag */
+void sprite_set_vertical_flip(struct Sprite* sprite, int vertical_flip) {
+    if (vertical_flip) {
+        /* set the bit */
+        sprite->attribute1 |= 0x2000;
+    } else {
+        /* clear the bit */
+        sprite->attribute1 &= 0xdfff;
+    }
+}
+
+/* change the vertical flip flag */
+void sprite_set_horizontal_flip(struct Sprite* sprite, int horizontal_flip) {
+    if (horizontal_flip) {
+        /* set the bit */
+        sprite->attribute1 |= 0x1000;
+    } else {
+        /* clear the bit */
+        sprite->attribute1 &= 0xefff;
+    }
+}
+
+/* change the tile offset of a sprite */
+void sprite_set_offset(struct Sprite* sprite, int offset) {
+    /* clear the old offset */
+    sprite->attribute2 &= 0xfc00;
+
+    /* apply the new one */
+    sprite->attribute2 |= (offset & 0x03ff);
+}
+
+/* setup the sprite image and palette */
+void setup_sprite_image() {
+    /* load the palette from the image into palette memory*/
+    memcpy16_dma((unsigned short*) sprite_palette, (unsigned short*) wall_obstacle_palette, PALETTE_SIZE);
+
+    /* load the image into sprite image memory */
+    memcpy16_dma((unsigned short*) sprite_image_memory, (unsigned short*) wall_obstacle_data, (wall_obstacle_width * wall_obstacle_height) / 2);
+}
+
+struct Wall {
+    /*the actual sprite attribute info */
+    struct Sprite* sprite;
+
+    /* the x and y position */
+    int x, y;
+    
+    /* which frame of animation it's on */
+    int frame;
+};
+
+/*initialize the wall*/
+void wall_init(struct Wall* wall){
+    wall->x = 200;
+    wall->y = 113;
+    wall->frame = 0;
+    wall->sprite = sprite_init(wall->x, wall->y, SIZE_32_64, 0, 0, wall->frame, 0);
+}
+
+/*probably going to have to include a wall_update function*/
+
 /* the main function */
 int main() {
     /* we set the mode to mode 0 with bg0 on */
@@ -163,6 +387,15 @@ int main() {
 
     /* setup the background 0 */
     setup_background();
+    
+    /*setup the sprite image data*/
+    setup_sprite_image();
+
+    /*clear all the sprites on screen now*/
+    sprite_clear();
+
+    struct Wall wall;
+    wall_init(&wall);
 
     /* set initial scroll to 0 */
     int xscroll = 0;
@@ -172,7 +405,7 @@ int main() {
 
     /* loop forever */
     while (1) {
-        /* scroll with the arrow keys */
+        /* scroll with the arrow keys 
         if (button_pressed(BUTTON_DOWN)) {
             yscroll++;
             yscroll2 += 1;
@@ -188,15 +421,19 @@ int main() {
         if (button_pressed(BUTTON_LEFT)) {
             xscroll--;
             xscroll2 -= 1;
-        }
+        }*/
+        
+        //xscroll++;
 
         /* wait for vblank before scrolling */
+         
         wait_vblank();
         *bg0_x_scroll = xscroll;
         *bg0_y_scroll = yscroll;
         *bg1_x_scroll = xscroll2;
         *bg1_y_scroll = yscroll2;
-
+        
+        sprite_update_all();
         /* delay some */
         delay(50);
     }
